cffe28e5c429ead276c2fd8042c3cf0af6793b71
aabid <115284244+aabidk20@users.noreply.github.com>
2025-07-02 00:45:55 +0530

    Telescope plugins setup

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,25 @@
 return {
   "nvim-telescope/telescope.nvim",
   config = function()
-    require("telescope").load_extension("fzf")
-    require("telescope").load_extension("import")
+    local telescope = require("telescope")
+    local lga_actions = require("telescope-live-grep-args.actions")
+    -- require("telescope").load_extension("fzf")
+    -- require("telescope").load_extension("import")
+    -- require("telescope").load_extension("live_grep_args")
+
+    telescope.setup({
+      live_grep_args = {
+        mappings = {
+          i = {
+            ["<C-'>"] = lga_actions.quote_prompt(),
+            ["<C-space>"] = lga_actions.to_fuzzy_refine,
+          },
+        },
+      },
+    })
+
+    telescope.load_extension("fzf")
+    telescope.load_extension("import")
+    telescope.load_extension("live_grep_args")
   end,
 }
