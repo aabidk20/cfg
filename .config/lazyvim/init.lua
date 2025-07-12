4ac3cf0f854385e062122f42acc7195288f61d83
aabid <115284244+aabidk20@users.noreply.github.com>
2025-07-13 00:13:23 +0530

    Color adjustments lazyvim

--- .config/lazyvim/init.lua
+++ .config/lazyvim/init.lua
@@ -8,3 +8,17 @@ require("mason").setup({
     "github:Crashdummyy/mason-registry",
   },
 })
+
+-- Color adjustments for NeoTree
+
+-- Dark separators
+-- vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#1a1b26", bg = "#1a1b26" })
+-- vim.api.nvim_set_hl(0, "VertSplit", { fg = "#1a1b26", bg = "#1a1b26" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "#1a1b26" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "#1a1b26" })
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#1a1b26" })
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#1a1b26" })
+
+--Lighter separators
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3b4261", bg = "#1a1b26" })
+vim.api.nvim_set_hl(0, "VertSplit", { fg = "#3b4261", bg = "#1a1b26" })
